dbcc59ba00a7e32bff071411cc0bf4007917aff3
Atrejo13 <atrejo@etsy.com>
2024-02-01 19:45:47 -0600

    Add multiple answer question

--- SwiftUIArchitectureTests/SwiftUI/MultipleSelectionStoreTests.swift
+++ SwiftUIArchitectureTests/SwiftUI/MultipleSelectionStoreTests.swift
@@ -6,35 +6,7 @@
 //
 
 import XCTest
-
-struct MultipleSelectionStore {
-    var options: [MultipleSelectionOption]
-    var canSubmit: Bool {
-//        options.first(where: { $0.isSelected }) != nil // other approach
-        !options.filter(\.isSelected).isEmpty
-    }
-    
-    private let handler: ([String]) -> Void
-    
-    internal init(options: [String], handler: @escaping ([String]) -> Void = { _ in}) {
-        self.options = options.map { MultipleSelectionOption(text: $0) }
-        self.handler = handler
-    }
-    
-    func submit() {
-        guard canSubmit else { return }
-        handler(options.filter(\.isSelected).map(\.text))
-    }
-}
-
-struct MultipleSelectionOption {
-    let text: String
-    var isSelected = false
-    
-    mutating func select() {
-        isSelected.toggle()
-    }
-}
+@testable import SwiftUIArchitecture
 
 final class MultipleSelectionStoreTests: XCTestCase {
 
@@ -65,7 +37,7 @@ final class MultipleSelectionStoreTests: XCTestCase {
     
     func test_submit_notifiesHandlerWithSelectedOptions() {
         var submittedOptions = [[String]]()
-        var sut = MultipleSelectionStore(options: ["o0", "01"], handler: {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"], handler: {
             submittedOptions.append($0)
         })
         
